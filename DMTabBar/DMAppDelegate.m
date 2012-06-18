29bf5fd0ba82cdac329affe6a8fe3b515d1f8492
daniele <malcom.mac@gmail.com>
2012-06-18 17:50:15 +0200

    Initial Commit

---
+++ DMTabBar/DMAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  DMAppDelegate.m
+//  DMTabBar
+//
+//  Created by malcom on 6/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DMAppDelegate.h"
+
+@implementation DMAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
